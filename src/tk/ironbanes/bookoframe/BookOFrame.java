7ee49e0cf1b47d8e22b80242bf605cfd26fb75eb
IronBanes <gavinator0000@gmail.com>
2020-12-27 03:17:48 -0600

    MySQL
    I'll be honest idk fully what survived the purge of SQL but I'm remaking the MySQL code. I was having so many issue and a null issue for some reason.

--- src/tk/ironbanes/bookoframe/BookOFrame.java
+++ src/tk/ironbanes/bookoframe/BookOFrame.java
@@ -1,24 +1,72 @@
 package tk.ironbanes.bookoframe;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BookOFrame extends JavaPlugin {
     private Connection connection;
+    public String host, database, username, password, table;
+    public int port;
+
+
 
     @Override
     public void onEnable(){
         getServer().getPluginManager().registerEvents(new EventHandler(), this);
         //getServer().getPluginManager().registerEvents(new EventHandler().onPlayerEntityInteract();, this);
         this.getCommand("bookoframe").setExecutor(new CommandBOF());
+        loadconfig();
+        mysqlSetup();
+
 
 
     }
+    public void loadconfig(){
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+    }
+
+    public void mysqlSetup(){
+        host = this.getConfig().getString("host");
+        port = this.getConfig().getInt("port");
+        database = this.getConfig().getString("database");
+        username = this.getConfig().getString("username");
+        password = this.getConfig().getString("password");
+        table = this.getConfig().getString("table");
+        try{
+            synchronized (this){
+                if(getConnection() != null && !getConnection().isClosed()){
+                    return;
+                }
+                Class.forName("com.mysql.jdbc.Driver");
+                setConnection(DriverManager.getConnection("jbdc:mysql://"+this.host+":"+ this.port+"/"
+                        + this.database, this.username,this.password));
+                Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"MYSQL CONNECTED");
+            }
+
+        }catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
 
 
-
-
+    }
     @Override
     public void onDisable() {
 
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
     }
 }
--- src/tk/ironbanes/bookoframe/EventHandler.java
+++ src/tk/ironbanes/bookoframe/EventHandler.java
@@ -1,20 +1,26 @@
 package tk.ironbanes.bookoframe;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.hanging.HangingPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EventHandler implements Listener
 {
+    BookOFrame plugin = BookOFrame.getPlugin(BookOFrame.class);
     //Checks if the entity the player is clicking on is an Item Frame
     @org.bukkit.event.EventHandler
     public void onPlayerEntityInteract(PlayerInteractEntityEvent event)
@@ -34,40 +40,62 @@ public class EventHandler implements Listener
             }
         }
     }
-
     @org.bukkit.event.EventHandler
-    public void onPlaceFrame(HangingPlaceEvent event){
+    public void onPlaceFrame(HangingPlaceEvent event)
+    {
         Player player = event.getPlayer();
         Entity entity = event.getEntity();
         player.sendMessage("Hello it is working");
         if (entity instanceof ItemFrame){
-            ItemStack itemstack = player.getInventory().getItemInMainHand();
-            if (itemstack.hasItemMeta()){
+            ItemStack itemframe = player.getInventory().getItemInMainHand();
+            if (itemframe.hasItemMeta()){
                 player.sendMessage("IT HAS META");
-                ItemMeta itemMeta = itemstack.getItemMeta();
+                ItemMeta itemMeta = itemframe.getItemMeta();
                 List<String> Lore = new ArrayList<>();
                 Lore.add(ChatColor.BLUE + ("This item frame allows for the copying of books"));
-                if (Lore.equals(itemMeta.getLore())){
+                if (itemMeta != null){
+                    if (Lore.equals(itemMeta.getLore())){
+                        World world = entity.getLocation().getBlock().getWorld();
+                        double x = entity.getLocation().getX();
+                        double y = entity.getLocation().getY();
+                        double z = entity.getLocation().getZ();
+                        player.sendMessage("AddBlockToSQL");
 
+
+                        plugin.getServer().broadcastMessage("ItemFrame Added");
+
+                        Bukkit.getConsoleSender().sendMessage("Lore exist");
+                    }
                 }
                 Lore.clear();
-
-
-
-
             }
-
-
-
+        }
+    }
+    @org.bukkit.event.EventHandler
+    public void onBreakFrame(HangingBreakEvent event)
+    {
+        Entity entity = event.getEntity();
+        if (entity instanceof ItemFrame){
+            World world = entity.getLocation().getBlock().getWorld();
+            double x = entity.getLocation().getX();
+            double y = entity.getLocation().getY();
+            double z = entity.getLocation().getZ();
+            /*if(checkSQLForBlock(world,x,y,z)){
+                removeBlockFromSQL(world,x,y,z);
+            }*/
         }
     }
 
-    /*@org.bukkit.event.EventHandler
-    public void onPlayerEntityPlacement(PlayerInteractEntityEvent event){
-        Player player = event.getPlayer();
-        Entity entity = event.getRightClicked();
 
-    }*/
+
+
+
+
+
+
+
+
+
 
 
 
